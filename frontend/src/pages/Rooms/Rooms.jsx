6813df0b7e4cf87c9072356ebc69ce522414816c
Amul Op <amulsharmarr67@gmail.com>
2023-03-20 23:52:16 +0530

    Room page Header implemented

--- frontend/src/pages/Rooms/Rooms.jsx
+++ frontend/src/pages/Rooms/Rooms.jsx
@@ -1,8 +1,40 @@
-// creating Rooms component
-const Rooms = () =>{
-    return <div> Rooms component</div>
-}
+import React from "react";
+import styles from "./Rooms.module.css";
 
+// creating Rooms component
+const Rooms = () => {
+  return (
+    <>
+      {/* container for room */}
+      <div className={styles.roomsContainer}>
+        {/* room header */}
+        <div className={styles.roomsHeader}>
+          {/* left items */}
+          <div className={styles.left}>
+            <span className={styles.spanText}>All Voice rooms </span>
+            {/* wrapper for input field -left */}
+            <div className={styles.inputWrapper}>
+              <img
+                className={styles.searchImage}
+                src="/images/search.png"
+              ></img>
+              <input className={styles.inputSearchField} type="text"></input>
+            </div>
+          </div>
+          {/* right items */}
+          <div className={styles.right}>
+            <button className={styles.startRoomButton}>
+              <img className={styles.roomImage} src="/images/room-icon.png"></img>
+              <span>Start a room!</span>
+            </button>
+          </div>
+        </div>
+
+        {/* room body elements */}
+      </div>
+    </>
+  );
+};
 
 // exporting rooms component
 export default Rooms;
