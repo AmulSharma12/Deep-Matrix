8a4011d4ce4e8ae16e983ef5fac2c12bb9bbb06e
Amul Op <amulsharmarr67@gmail.com>
2023-03-22 23:38:49 +0530

    Room Card component implemented

---
+++ frontend/src/components/Shared/RoomCard/RoomCard.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import styles from "./RoomCard.module.css";
+
+//RoomCard component
+const RoomCard = ({ room }) => {
+  return (
+    <>
+      {/* Card component */}
+      <div className={styles.card}>
+        {/* topic -Card */}
+        <h3 className={styles.topic}>{room.topic}</h3>
+
+        {/* speakers container */}
+        <div className={styles.speakers}>
+          {/* rendering avatars presentt in speakers array by looping it */}
+          <div className={styles.avatars}>
+            {room.speakers.map((speaker) => (
+              <img src={speaker.avatar} alt="speaker-avatar"></img>
+            ))}
+          </div>
+          {/* rendering names presentt in speakers array by looping it */}
+          <div className={styles.names}>
+            {room.speakers.map((speaker) => (
+              <div className={styles.nameWrapper}>
+                <span>{speaker.name}</span>
+                <img src="/images/chat-bubble.png" alt="img"></img>
+              </div>
+              // <img src={speaker.avatar} alt="speaker-avatar"></img>
+            ))}
+          </div>
+        </div>
+
+        {/* Total participants */}
+        <div className={styles.peopleCount}>
+          <span>{room.totalPeople}</span>
+          <img src="/images/user-icon.png" alt="total-count"></img>
+        </div>
+      </div>
+    </>
+  );
+};
+
+//exporting RoomCard component
+export default RoomCard;
--- frontend/src/pages/Rooms/Rooms.jsx
+++ frontend/src/pages/Rooms/Rooms.jsx
@@ -1,5 +1,78 @@
 import React from "react";
+import RoomCard from "../../components/Shared/RoomCard/RoomCard";
 import styles from "./Rooms.module.css";
+
+//dummy data for the room list
+const rooms = [
+  {
+    id: 1,
+    topic: "Which framework best for frontend ?",
+    speakers: [
+      {
+        id: 1,
+        name: "John Doe",
+        avatar: "/images/profile.png",
+      },
+      {
+        id: 2,
+        name: "Jane Doe",
+        avatar: "/images/profile.png",
+      },
+    ],
+    totalPeople: 40,
+  },
+  {
+    id: 3,
+    topic: "What’s new in machine learning?",
+    speakers: [
+      {
+        id: 1,
+        name: "John Doe",
+        avatar: "/images/profile.png",
+      },
+      {
+        id: 2,
+        name: "Jane Doe",
+        avatar: "/images/profile.png",
+      },
+    ],
+    totalPeople: 40,
+  },
+  {
+    id: 4,
+    topic: "Why people use stack overflow?",
+    speakers: [
+      {
+        id: 1,
+        name: "John Doe",
+        avatar: "/images/profile.png",
+      },
+      {
+        id: 2,
+        name: "Jane Doe",
+        avatar: "/images/profile.png",
+      },
+    ],
+    totalPeople: 40,
+  },
+  {
+    id: 5,
+    topic: "Artificial inteligence is the future?",
+    speakers: [
+      {
+        id: 1,
+        name: "John Doe",
+        avatar: "/images/profile.png",
+      },
+      {
+        id: 2,
+        name: "Jane Doe",
+        avatar: "/images/profile.png",
+      },
+    ],
+    totalPeople: 40,
+  },
+];
 
 // creating Rooms component
 const Rooms = () => {
@@ -24,13 +97,27 @@ const Rooms = () => {
           {/* right items */}
           <div className={styles.right}>
             <button className={styles.startRoomButton}>
-              <img className={styles.roomImage} src="/images/room-icon.png"></img>
+              <img
+                className={styles.roomImage}
+                src="/images/room-icon.png"
+              ></img>
               <span>Start a room!</span>
             </button>
           </div>
         </div>
 
         {/* room body elements */}
+        <div className={styles.roomList}>
+          {/* rendering the rooms list */}
+          {rooms.map((room) => (
+            <>
+              <RoomCard key={room.id} room={room} />
+              <RoomCard key={room.id} room={room} />
+              <RoomCard key={room.id} room={room} />
+              <RoomCard key={room.id} room={room} />
+            </>
+          ))}
+        </div>
       </div>
     </>
   );
